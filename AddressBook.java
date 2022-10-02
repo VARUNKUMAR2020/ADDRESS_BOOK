65ea8c5c0f44356508d6a76f5b1ed7eab4721b41
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-10-02 15:52:56 +0530

    ADD: CREATION OF THE CONTACTS

--- AddressBook.java
+++ AddressBook.java
@@ -1,7 +1,42 @@
 package AddressBook;
 
+import java.util.Scanner;
+
 public class AddressBook {
+
 	public static void main(String[] args) {
 		System.out.println("WELCOME TO ADDRESS BOOK");
+		Scanner fname = new Scanner(System.in);
+		System.out.println("ENTER THE FIRST NAME: ");
+		String firstName = fname.nextLine();
+		Scanner lname = new Scanner(System.in);
+		System.out.println("ENTER THE LAST NAME: ");
+		String lastName = fname.nextLine();
+		Scanner add = new Scanner(System.in);
+		System.out.println("ENTER THE ADDRESS: ");
+		String address = add.nextLine();
+		Scanner City = new Scanner(System.in);
+		System.out.println("ENTER THE CITY: ");
+		String city = City.nextLine();
+		Scanner State = new Scanner(System.in);
+		System.out.println("ENTER THE STATE: ");
+		String state = State.nextLine();
+		Scanner Email = new Scanner(System.in);
+		System.out.println("ENTER THE EMAIL: ");
+		String email = Email.nextLine();
+		Scanner zipcode = new Scanner(System.in);
+		System.out.println("ENTER THE ZIPCODE: ");
+		int zip = zipcode.nextInt();
+		Scanner cell = new Scanner(System.in);
+		System.out.println("ENTER THE PHONE NUMBER: ");
+		long phoneNumber = cell.nextLong();
+		System.out.println("FIRST NAME: " + firstName);
+		System.out.println("LAST NAME: " + lastName);
+		System.out.println("ADRESS: " + address);
+		System.out.println("CITY: " + city);
+		System.out.println("STATE: " + state);
+		System.out.println("EMAIL: " + email);
+		System.out.println("ZIPCODE: " + zip);
+		System.out.println("PHONE NUMBER: " + phoneNumber);
 	}
 }
